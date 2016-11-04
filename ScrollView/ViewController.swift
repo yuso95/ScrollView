8dc6239460f5f23c572d7b72aaab958e8abf7c01
Ousmane Y. Abdou <Yuso@Younoussas-MacBook-Pro.local>
2016-11-04 16:44:57 -0400

    Finished version of Mark scrollView

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]()
+    
+    var contentSize: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let scrollWidth = scrollView.frame.size.width
+        
+        for x in 0...2 {
+            
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.height / 2) - 75, width: 150, height: 150)
+            
+            contentSize += newX
+        }
+        
+        scrollView.contentSize = CGSize(width: contentSize, height: view.frame.size.height)
+        
+        scrollView.clipsToBounds = false
     }
-
-
 }
 
